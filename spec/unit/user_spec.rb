3187f3269b9bb800b9d81cae4dfadad5db537402
toby676 <tjcmui@gmail.com>
2016-09-01 10:45:21 +0100

    user test pass

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,9 +1,10 @@
 describe User do
 
-  let(:user) {described_class.new}
+  let(:user) {described_class}
 
   it 'user can be created' do
     user.create(email: 'toby@toby.com', password: 'hello123')
+    expect(user.count).to equal 1
   end
 
 end
